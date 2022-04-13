22578ad41aededd48383b155afb2b81badaf1eee
Daniel Joudat <danieljoudat@gmail.com>
2022-04-13 17:35:08 +0300

    print some info out
    Signed-off-by: Daniel Joudat <danieljoudat@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,13 @@ echo 'Running M3O Generator Action'
 echo "Target: ${INPUT_TARGET}"
 WORK_DIR=$(pwd)
 echo "Working directory ${WORK_DIR}"
+
 cd ${INPUT_SERVICES_PATH}
 m3o-client-gen ${INPUT_TARGET}
+
+ls -al
+
+cd ..
+ls -al
 
 # echo "::set-output name=success::$success"
