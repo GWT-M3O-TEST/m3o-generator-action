d1f29fc61d5b36ca8bb448d0e3e95e5f0eaced7a
Daniel Joudat <danieljoudat@gmail.com>
2022-04-16 18:17:36 +0300

    add special case for golang in the entrypoint.sh
    Signed-off-by: Daniel Joudat <danieljoudat@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,13 @@ echo "Working directory ${WORK_DIR}"
 cd ${INPUT_SERVICES_PATH}
 m3o-client-gen ${INPUT_TARGET}
 
+if [ ${INPUT_TARGET} = 'go' ]
+then
+    echo 'Formating m3o-go clients and examples'
+    go fmt ./...
+    
+    echo 'Testing m3o-go clients'
+    go test -v ./...
+fi
+
 # echo "::set-output name=success::$success"
